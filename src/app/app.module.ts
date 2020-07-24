e30a1f7a504ae6f2d0d97a38a1bb4002b45f4ba1
Saul <saulmme12@gmail.com>
2020-07-24 15:33:37 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InscripcionComponent } from './inscripcion/inscripcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InscripcionComponent
   ],
   imports: [
     BrowserModule,
